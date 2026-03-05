cebbca224eee9958534d20608298b768923665bf
harrykumarr <harikumarreddy1221@gmail.com>
2026-03-05 19:06:25 +0530

    Fix CORS issue

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ mongoose.connect(MONGO_URI)
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
+    origin: function(origin, callback) {
+        callback(null, true);
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
